d1cbb7ec8bb5a5ab2bd5467c596557c0f4748d87
Sai Balaram <Kapavarapu.SaiBalaram@philips.com>
2021-03-13 01:45:41 +0530

    as a final step in setting up redux,store has been provided to App.Also,errors in actions hv been rectified

--- kt-two/src/redux/actions.js
+++ kt-two/src/redux/actions.js
@@ -1,4 +1,4 @@
-const ADD_CAKE_TO_CART= 'ADD_CAKE_TO_CART'
+export const ADD_CAKE_TO_CART= 'ADD_CAKE_TO_CART'
 export const addCakeToCartActionHandler= (flavour)=>{
     return ({type:ADD_CAKE_TO_CART,flavour:flavour})
 }
--- kt-two/src/redux/cakeReducer.js
+++ kt-two/src/redux/cakeReducer.js
@@ -1,7 +1,7 @@
-
+import {ADD_CAKE_TO_CART} from './actions'
 export const cakeReducer=(state,action)=>{
     switch(action.type){
-        CASE ADD_CAKE_TO_CART : {
+        case ADD_CAKE_TO_CART : {
             return (
                     {...state,
                         cakes:[...state.cakes,{flavour:action.flavour}]
