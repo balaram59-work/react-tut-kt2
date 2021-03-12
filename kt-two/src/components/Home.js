58390b5ac455af579da7121762922252d77a23df
Sai Balaram <Kapavarapu.SaiBalaram@philips.com>
2021-03-13 00:59:22 +0530

    added Home and Header components but nothing happens clicking the button

---
+++ kt-two/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+           
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{}
+                        }>
+                        Add Cake To Cart</button>
+                </div>
+        </div>
+    )
+}
+export default Home
